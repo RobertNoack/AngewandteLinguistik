46eb59b956ff6ebb38c177a8535f353a5a9f94e9
le8266 <ramon.bernert@gmail.com>
2015-07-02 10:00:59 +0200

    if city not found split the name and search again

--- src/geonames.py
+++ src/geonames.py
@@ -27,29 +27,38 @@ class Location(object):
 	def getUrl(self):
 		return MAPS + str(self.lat) + ',' + str(self.lng)
 
-def fetchJson(method, params):
-	uri = DOMAIN + '%s?%s' % (method, urllib.parse.urlencode(params))
-	response = urllib.request.urlopen(uri)
-	content = response.read()
-	js = json.loads(content.decode("utf8"))
-	return js
+	def fetchJson(method, params):
+		uri = DOMAIN + '%s?%s' % (method, urllib.parse.urlencode(params))
+		response = urllib.request.urlopen(uri)
+		content = response.read()
+		js = json.loads(content.decode("utf8"))
+		return js
 
-def getLocation(city):
-	method = 'postalCodeSearchJSON'
-	params = {'placename':city, 'username':USERNAME}
-	results = fetchJson(method, params)
+	def getLocation(city):
+		method = 'postalCodeSearchJSON'
+		params = {'placename':city, 'username':USERNAME}
+		results = Location.fetchJson(method, params)
 
-	if('postalCodes' in results):
-		if(len(results['postalCodes']) > 0):
-			postalCode = results['postalCodes'][0]
-			if ('placeName' in postalCode and 'lng' in postalCode and 'lat' in postalCode):
-				return Location(postalCode['placeName'], postalCode['lng'], postalCode['lat'])
+		if('postalCodes' in results):
+			if(len(results['postalCodes']) > 0):
+				postalCode = results['postalCodes'][0]
+				if ('placeName' in postalCode and 'lng' in postalCode and 'lat' in postalCode):
+					return Location(postalCode['placeName'], postalCode['lng'], postalCode['lat'])
+			else:
+				if ' ' in city:
+					for part in city.split(' '):
+						return Location.getLocation(part)
+						if(l is not None):
+							return l
 
-	return None
+		return None
 
 if __name__ == '__main__':
-	l = getLocation('Leipzig')
-	if(l is None):
-		print('Not found')
+	if(len(sys.argv) > 0):
+		l = Location.getLocation(sys.argv[len(sys.argv) - 1])
+		if(l is None):
+			print('Not found')
+		else:
+			print("name: " + l.getName() + " lng: " + str(l.getLng()) + " lat: " + str(l.getLat()) + " url: " + str(l.getUrl()))
 	else:
-		print("name: " + l.getName() + " lng: " + str(l.getLng()) + " lat: " + str(l.getLat()) + " url: " + str(l.getUrl()))
+		print('No location')
